f5bb19fd984dbf89ba939adf342bd7dbe615f426
Nuo <62932656+Nuo27@users.noreply.github.com>
2022-04-29 15:55:05 +0800

    TestScene build / PlayerControl

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,41 +4,67 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Camera cam;
+    public bool isGrounded;
+    public Rigidbody rb;
+    public float jumpForce;
+    public float mouseSensitivity;
+    public float moveSpeed;
+    private float moveHorizontal;
+    private float moveVertical;
+    private float moveX;
+    private float moveY;
+    private float rotationY = 0.0f;
+
+    public GameObject target;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        jumpForce = 4f;
+        mouseSensitivity = 4f;
+        moveSpeed = 4f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //get keyboard input
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        //mouse movement
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-        //move the player
-        transform.Translate(new Vector3(horizontal, 0, vertical) * Time.deltaTime * 10);
-        //rotate the player
-        transform.Rotate(new Vector3(0, mouseX, 0));
-        //move the player with wasd
-        if(Input.GetKey(KeyCode.W))
+        //movement inputs
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+        moveX = Input.GetAxis("Mouse X");
+        moveY = Input.GetAxis("Mouse Y");
+        rotationY -= moveY * mouseSensitivity;
+        rotationY = Mathf.Clamp(rotationY, -90f, 90f);
+        //movement
+        target.transform.Rotate(0, moveX * mouseSensitivity, 0);
+        cam.transform.localRotation = Quaternion.Euler(rotationY, 0, 0);
+        target.transform.Translate(Vector3.right * moveHorizontal * moveSpeed * Time.deltaTime, Space.Self);
+        target.transform.Translate(Vector3.forward * moveVertical * moveSpeed * Time.deltaTime, Space.Self);
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            transform.Translate(new Vector3(0, 0, 1) * Time.deltaTime * 10);
+            rb.AddForce(new Vector3(0, 2.0f, 0) * jumpForce, ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.S))
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "ground")
         {
-            transform.Translate(new Vector3(0, 0, -1) * Time.deltaTime * 10);
+            isGrounded = true;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * 10);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(new Vector3(1, 0, 0) * Time.deltaTime * 10);
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "ground")
+        { 
+            isGrounded = false;
         }
     }
 }
