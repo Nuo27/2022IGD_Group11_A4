41461fcfda9160e904aab379375fd800f7d0ace1
Nuo <62932656+Nuo27@users.noreply.github.com>
2022-05-01 15:48:13 +0800

    Restructured UI elements and their trigger

--- Assets/Scripts/Manager/UIManager.cs
+++ Assets/Scripts/Manager/UIManager.cs
@@ -7,16 +7,21 @@ public class UIManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject LevelMessageHolder;
+    public GameObject ControlMessageHolder;
     public GameObject MessageHolder;
     public TMPro.TextMeshProUGUI ObjectiveText;
+    public TMPro.TextMeshProUGUI CurrentLevelText;
     void Start()
     {
+        
         ObjectiveText.text = SetObjectiveText(LevelManager.GetCurrentLevelIndex());
+        CurrentLevelText.text = SetCurrentLevelText(LevelManager.GetCurrentLevelIndex());
         Invoke("KillLevelStartMessages", 3f);
         DontDestroyOnLoad(gameObject);
     }
     void KillLevelStartMessages(){
         LevelMessageHolder.SetActive(false);
+        ControlMessageHolder.SetActive(false);
         //Destroy(LevelMessageHolder);
     }
     // Update is called once per frame
@@ -52,7 +57,24 @@ public class UIManager : MonoBehaviour
                 return "Find the red Flower";
             default:
                 return "";
-
+        }
+    }
+    string SetCurrentLevelText(int CurrentLevelIndex){
+        switch(CurrentLevelIndex){
+            case 1:
+                return "Level 1";
+            case 2:
+                return "Level 2";
+            case 3:
+                return "Level 3";
+            case 4:
+                return "Level 4";
+            case 5:
+                return "Level Tutorial";
+            case 6:
+                return "level Test";
+            default:
+                return "";
         }
     }
     public void OnClickTutorialDone(){
